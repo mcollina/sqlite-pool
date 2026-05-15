fb8f11bf718e2ca49a1fbd2e44f634a67bbddc34
Matteo Collina <hello@matteocollina.com>
2026-05-15 18:17:01 +0100

    Vendor sqlite-sync implementation (#271)
    * Vendor sqlite-sync implementation
    
    * Ignore local agent state

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,179 @@
-import sql, { SQLQuery, isSqlQuery } from "@databases/sql";
-import connect, {
-  DatabaseConnection as SyncDatabaseConnection,
-} from "@databases/sqlite-sync";
 import createBaseConnectionPool, {
   ConnectionPool,
   PoolConnection,
   PoolOptions,
 } from "@databases/connection-pool";
 import { escapeSQLiteIdentifier } from "@databases/escape-identifier";
+import sql, { SQLQuery, isSqlQuery } from "@databases/sql";
+import splitSqlQuery from "@databases/split-sql-query";
 import { once } from "events";
+import DatabaseConstructor = require("better-sqlite3");
 
 export type { SQLQuery };
 export { sql, isSqlQuery };
 
-type connectParameters = Parameters<typeof connect>;
+type DatabaseOptions = DatabaseConstructor.Options;
+type BetterSqlite3Database = DatabaseConstructor.Database;
 
-type DatabaseOptions = connectParameters[1];
+const IN_MEMORY = ":memory:";
+
+// Synchronous adapter vendored from @databases/sqlite-sync@3.0.0.
+// Copyright (c) 2019 Forbes Lindesay, MIT License.
+const sqliteFormat = {
+  escapeIdentifier: (str: string) => escapeSQLiteIdentifier(str),
+  formatValue: (value: unknown) => ({ placeholder: "?", value }),
+};
+
+interface SyncDatabaseTransaction {
+  query(query: SQLQuery): any[];
+  query(query: SQLQuery[]): any[][];
+  queryStream(query: SQLQuery): IterableIterator<any>;
+}
+
+interface SyncDatabaseConnection extends SyncDatabaseTransaction {
+  tx<T>(fn: (db: SyncDatabaseTransaction) => T): T;
+  dispose(): void;
+}
+
+class SyncDatabaseTransactionImplementation implements SyncDatabaseTransaction {
+  #database: BetterSqlite3Database;
+
+  constructor(database: BetterSqlite3Database) {
+    this.#database = database;
+  }
+
+  query(query: SQLQuery): any[];
+  query(query: SQLQuery[]): any[][];
+  query(query: SQLQuery | SQLQuery[]): any[] | any[][] {
+    return runQuery(query, this.#database);
+  }
+
+  queryStream(query: SQLQuery): IterableIterator<any> {
+    return runQueryStream(query, this.#database);
+  }
+}
+
+class SyncDatabaseConnectionImplementation implements SyncDatabaseConnection {
+  #database: BetterSqlite3Database;
+  #begin: DatabaseConstructor.Statement;
+  #commit: DatabaseConstructor.Statement;
+  #rollback: DatabaseConstructor.Statement;
+
+  constructor(filename: string, options: DatabaseOptions) {
+    this.#database = new DatabaseConstructor(filename, options);
+    this.#begin = this.#database.prepare("BEGIN");
+    this.#commit = this.#database.prepare("COMMIT");
+    this.#rollback = this.#database.prepare("ROLLBACK");
+  }
+
+  query(query: SQLQuery): any[];
+  query(query: SQLQuery[]): any[][];
+  query(query: SQLQuery | SQLQuery[]): any[] | any[][] {
+    return runQuery(query, this.#database);
+  }
+
+  queryStream(query: SQLQuery): IterableIterator<any> {
+    return runQueryStream(query, this.#database);
+  }
+
+  tx<T>(fn: (db: SyncDatabaseTransaction) => T): T {
+    this.#begin.run();
+    try {
+      const result = fn(
+        new SyncDatabaseTransactionImplementation(this.#database),
+      );
+      this.#commit.run();
+      return result;
+    } catch (ex) {
+      this.#rollback.run();
+      throw ex;
+    }
+  }
+
+  dispose(): void {
+    this.#database.close();
+  }
+}
+
+function connect(
+  filename: string = IN_MEMORY,
+  options: DatabaseOptions = {},
+): SyncDatabaseConnection {
+  return new SyncDatabaseConnectionImplementation(filename, options);
+}
+
+function runQuery(query: SQLQuery, database: BetterSqlite3Database): any[];
+function runQuery(query: SQLQuery[], database: BetterSqlite3Database): any[][];
+function runQuery(
+  query: SQLQuery | SQLQuery[],
+  database: BetterSqlite3Database,
+): any[] | any[][];
+function runQuery(
+  query: SQLQuery | SQLQuery[],
+  database: BetterSqlite3Database,
+): any[] | any[][] {
+  if (Array.isArray(query)) {
+    for (const q of query) {
+      if (!isSqlQuery(q)) {
+        throw new Error(
+          "Invalid query, you must use @databases/sql to create your queries.",
+        );
+      }
+    }
+    return query.map((q) => runOneQuery(q, database));
+  }
+
+  if (!isSqlQuery(query)) {
+    throw new Error(
+      "Invalid query, you must use @databases/sql to create your queries.",
+    );
+  }
+
+  const queries = splitSqlQuery(query);
+  const lastQuery = queries.pop();
+  if (!lastQuery) {
+    return [];
+  }
+
+  for (const q of queries) {
+    runOneQuery(q, database);
+  }
+  return runOneQuery(lastQuery, database);
+}
+
+function runOneQuery(query: SQLQuery, database: BetterSqlite3Database): any[] {
+  const { text, values } = query.format(sqliteFormat);
+  const statement = database.prepare(text);
+  try {
+    return statement.all(...values);
+  } catch (_err) {
+    const err = _err as Error;
+    // TODO we should be able to catch this before calling all()
+    if (err.message.indexOf("This statement does not return data") >= 0) {
+      statement.run(...values);
+      return [];
+    }
+    throw err;
+  }
+}
+
+function* runQueryStream(
+  query: SQLQuery,
+  database: BetterSqlite3Database,
+): IterableIterator<any> {
+  if (!isSqlQuery(query)) {
+    throw new Error(
+      "Invalid query, you must use @databases/sql to create your queries.",
+    );
+  }
+
+  const { text, values } = query.format(sqliteFormat);
+  const statement = database.prepare(text);
+  const rows = statement.iterate(...values);
+  for (const row of rows || []) {
+    yield row;
+  }
+}
 
 export interface DatabaseTransaction {
   query(query: SQLQuery): Promise<any[]>;
